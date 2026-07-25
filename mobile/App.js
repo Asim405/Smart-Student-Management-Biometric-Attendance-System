50b9258cd6d8b3c9217d49b606d44a78290a3906
Asim405 <asimsultan405@gmail.com>
2026-07-25 19:34:09 +0500

    frontend one insertion in app.js

--- mobile/App.js
+++ mobile/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from './context/AuthContext';
 import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
+  
   return (
     <AuthProvider>
       <StatusBar style="dark" />
