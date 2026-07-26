ac8d051de7b8bb4c593276bf14a6410851a8fbe2
Asim405 <asimsultan405@gmail.com>
2026-07-26 21:07:27 +0500

    metro code changed

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+// Enable package exports resolution fallback for Metro
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = withNativeWind(config, { input: './global.css' });
