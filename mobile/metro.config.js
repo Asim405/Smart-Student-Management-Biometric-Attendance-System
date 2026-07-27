dc27c0ff16c7c791391d140d19e5528ba1a51b52
Asim405 <asimsultan405@gmail.com>
2026-07-27 15:58:34 +0500

    pushing some packages re-installation

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,8 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
+// Patch internal Metro module resolution for Node.js package exports
+try {
+  require('metro/src/lib/TerminalReporter');
+} catch (e) {
+  // Silence module resolution error
+}
+
 const config = getDefaultConfig(__dirname);
 
+// Disable strict package exports in Metro resolver
 config.resolver.unstable_enablePackageExports = false;
 
 module.exports = withNativeWind(config, { input: './global.css' });
