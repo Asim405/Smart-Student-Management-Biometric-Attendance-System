0dc3fb0f1d42a023aea1ab589c82470767fba34d
Asim405 <asimsultan405@gmail.com>
2026-07-28 22:03:11 +0500

    chanings in app.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,15 +1,27 @@
 import './global.css';
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './context/AuthContext';
 import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
-  
   return (
-    <AuthProvider>
-      <StatusBar style="dark" />
-      <RootNavigator />
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <RootNavigator />
+        </View>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+});
