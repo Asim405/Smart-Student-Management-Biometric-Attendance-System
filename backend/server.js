100a5c742ce2d33ce60a8bc613b237742351d7b0
Asim405 <asimsultan405@gmail.com>
2026-07-29 19:01:36 +0500

    backend /server.js

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,5 @@ initSockets(io);
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  console.log(`Health check endpoint: http://localhost:${PORT}/api/health`);
   
 });
