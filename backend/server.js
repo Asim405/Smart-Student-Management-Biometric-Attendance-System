cebea7411790fd9b4064f8d7304a60fd56e1d427
Asim405 <asimsultan405@gmail.com>
2026-07-29 19:02:36 +0500

    soccket adding in server.js

--- backend/server.js
+++ backend/server.js
@@ -41,5 +41,6 @@ initSockets(io);
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  console.log(`Socket.io listening on port ${PORT}`);
   
 });
